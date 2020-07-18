d75fe3e9da0ee1311e6ad6eb7a7b09c33c7967ac
Mahek <maheknakhua11@gmail.com>
2020-07-19 05:20:01 +0530

    spinner

--- lib/screens/tile.dart
+++ lib/screens/tile.dart
@@ -14,31 +14,26 @@ class tile extends StatefulWidget {
   _tileState createState() => new _tileState();
 }
 
-
-class _tileState extends State<tile> with TickerProviderStateMixin{
-  int _state=0;
-
-
-  void animateButton(){
-    setState(() {
-      _state=1;
-    });
-    Timer(Duration(milliseconds: 3300),(){
-      setState(() {
-        _state=2;
-      });
-
-  });
-}
-
-
-
+class _tileState extends State<tile> {
+//with TickerProviderStateMixin
+//  void animateButton(){
+//    setState(() {
+//      _state=1;
+//    });
+//    Timer(Duration(milliseconds: 3300),(){
+//      setState(() {
+//        _state=2;
+//      });
+//
+//  });
+//}
 
   final ModelManager modelManager = FirebaseLanguage.instance.modelManager();
+  int _state = 1;
   bool downloadState = false;
   String currentStatus;
+
   Future<void> checkStatus(String langCode) async {
-    bool status = false;
     List<String> downloadedList = await modelManager.viewModels();
     print(downloadedList);
     for (int i = 0; i < downloadedList.length; i++) {
@@ -49,17 +44,21 @@ class _tileState extends State<tile> with TickerProviderStateMixin{
     }
   }
 
-  Widget trail_icon(){
-    if(_state==0){
-      return Icon(Icons.arrow_downward);
-    }
-    else if (_state==1){
+  Widget trail_icon() {
+    if (_state == 0) {
       return CircularProgressIndicator(
         valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
       );
-    }
-    else{
-      return Icon(Icons.delete);
+    } else if (_state == 1) {
+      return Icon(
+        Icons.arrow_downward,
+        color: Colors.blue,
+      );
+    } else if (_state == 2) {
+      return Icon(
+        Icons.delete,
+        color: Colors.black,
+      );
     }
   }
 
@@ -73,35 +72,34 @@ class _tileState extends State<tile> with TickerProviderStateMixin{
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(
-        widget.language,
-        style: downloadState ? offline : online,
-      ),
-      //trailing: Icon(Icons.arrow_downward),
-      onTap: () async {
-        if (_state==0){
-          animateButton();
-        }
-        if (downloadState == false) {
-          currentStatus = await modelManager.downloadModel(widget.languageCode);
-          print('downloading ... $currentStatus');
-        } else {
-          currentStatus = await modelManager.deleteModel(widget.languageCode);
-          print('deleting ... $currentStatus');
-        }
-        setState(() {
-          downloadState = !downloadState;
-        });
-      },
-
-
-
-//      trailing: Icon(
-//        downloadState ? Icons.delete : Icons.arrow_downward,
-//        color: downloadState ? Colors.black : Colors.blue,
-
-    );
-
+        title: Text(
+          widget.language,
+          style: downloadState ? offline : online,
+        ),
+        //trailing: Icon(Icons.arrow_downward),
+        onTap: () async {
+          if (_state == 1 || _state == 2) {
+            setState(() {
+              _state = 0;
+            });
+          }
+          if (downloadState == false) {
+            currentStatus =
+                await modelManager.downloadModel(widget.languageCode);
+            print('downloading ... $currentStatus');
+            setState(() {
+              _state = 2;
+              downloadState = !downloadState;
+            });
+          } else {
+            currentStatus = await modelManager.deleteModel(widget.languageCode);
+            print('deleting ... $currentStatus');
+            setState(() {
+              _state = 1;
+              downloadState = !downloadState;
+            });
+          }
+        },
+        trailing: trail_icon());
   }
-
 }
