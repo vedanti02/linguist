b98ac91c50dad0cc9e62e4baa101550fb628ca77
Mahek <maheknakhua11@gmail.com>
2020-07-18 22:31:00 +0530

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'screens/mainscreen.dart';
-import 'screens/camrea.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +9,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        initialRoute:MainScreen.id,
-        routes: {
-          MainScreen.id: (context) => MainScreen(),
-          Camera.id: (context) => Camera(),
-        }
-    );
+    return MaterialApp(initialRoute: MainScreen.id, routes: {
+      MainScreen.id: (context) => MainScreen(),
+//          Camera.id: (context) => Camera(),
+    });
   }
-
 }
